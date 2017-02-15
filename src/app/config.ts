62a145b7b74b0d4e5bf90836063677dc01154123
Mankota Sparrow <Mankota Sparrow>
2017-02-14 22:50:48 -0700

    my first commit

--- src/app/config.ts
+++ src/app/config.ts
@@ -7,7 +7,8 @@ export class Config {
 
 	// url of the chat server
 	// for local development it will be something like http://192.168.0.214:9000/
-	public static server = 'https://ionic-video-chat-server.herokuapp.com/';
+	//public static server = 'https://ionic-video-chat-server.herokuapp.com/';
+	public static server = 'https://worlddove.herokuapp.com/';
 	//public static server = 'http://192.168.0.214:5000/';
 
 	// enables or disables chat sounds. usefull for development
